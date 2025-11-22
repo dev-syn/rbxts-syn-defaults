73a6f174e0d90c4f8998bb44d759428953b7b7bd
Casey <103451081+dev-syn@users.noreply.github.com>
2025-11-22 13:51:42 -0500

    init

---
+++ tests/App.tsx
@@ -0,0 +1,12 @@
+import React from '@rbxts/react';
+
+export const App = () => {
+	return (
+		<screengui ResetOnSpawn={false} DisplayOrder={10}>
+			<scrollingframe Size={new UDim2(1,0,1,0)} CanvasSize={new UDim2(0,0,2,0)}>
+				<uigridlayout CellSize={new UDim2(0,200,0,200)}/>
+				
+			</scrollingframe>
+		</screengui>
+	);
+};
