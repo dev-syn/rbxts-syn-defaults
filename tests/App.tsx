bd18963a8d5fe930e40c07bfbbaf8fe151dd7af6
Casey <103451081+dev-syn@users.noreply.github.com>
2025-11-22 19:31:11 -0500

    Add test CloseBtn component

--- tests/App.tsx
+++ tests/App.tsx
@@ -1,4 +1,5 @@
 import React from '@rbxts/react';
+import { CloseBtn } from './CloseBtn';
 
 export const App = () => {
 	return (
@@ -6,6 +7,8 @@ export const App = () => {
 			<scrollingframe Size={new UDim2(1,0,1,0)} CanvasSize={new UDim2(0,0,2,0)}>
 				<uigridlayout CellSize={new UDim2(0,200,0,200)}/>
 				
+				<CloseBtn onClick={() => print("Close button was pressed!")} />
+
 			</scrollingframe>
 		</screengui>
 	);
