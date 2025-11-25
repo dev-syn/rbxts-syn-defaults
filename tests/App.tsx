13bd5f38539b1fd23041201e6f550ef586fac86e
Casey <103451081+dev-syn@users.noreply.github.com>
2025-11-24 19:51:23 -0500

    Change App size and position

--- tests/App.tsx
+++ tests/App.tsx
@@ -4,7 +4,12 @@ import { CloseBtn } from './CloseBtn';
 export const App = () => {
 	return (
 		<screengui ResetOnSpawn={false} DisplayOrder={10}>
-			<scrollingframe Size={new UDim2(1,0,1,0)} CanvasSize={new UDim2(0,0,2,0)}>
+			<scrollingframe
+				AnchorPoint={new Vector2(0.5,0.5)}
+				Size={new UDim2(0.65,0,0.55,0)}
+				CanvasSize={new UDim2(0,0,1.5,0)}
+				Position={new UDim2(0.5,0,0.5,0)}
+			>
 				<uigridlayout CellSize={new UDim2(0,200,0,200)}/>
 				
 				<CloseBtn onClick={() => print("Close button was pressed!")} />
