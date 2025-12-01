f830a4afd6bee03ccaa9181fe8d6ae15db3bec7a
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-01 15:46:31 -0500

    Add starting boiler code for testing SelectableGroup

---
+++ tests/components/SelectableGroup.tsx
@@ -0,0 +1,15 @@
+import React from '@rbxts/react';
+
+interface SelectableGroupProps {
+
+}
+
+export function SelectableGroup(props: SelectableGroupProps) {
+	return (
+		<scrollingframe
+		>
+			<uilistlayout/>
+			<textbutton></textbutton>
+		</scrollingframe>
+	);
+}
