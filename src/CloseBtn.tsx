0f58c90ef0805a8a4e3f9d10eb078fa47d7b6727
Casey <103451081+dev-syn@users.noreply.github.com>
2025-11-30 22:58:54 -0500

    Refactor to act as a container with luminance contrast support

--- src/CloseBtn.tsx
+++ src/CloseBtn.tsx
@@ -1,62 +1,90 @@
-import React, { useRef } from '@rbxts/react';
+import React, { forwardRef, Ref, useMemo, useRef } from '@rbxts/react';
+import { TweenService } from '@rbxts/services';
 
 const DEFAULT_CLOSE: string = "http://www.roblox.com/asset/?id=6031094678";
 const DEFAULT_SIZE: UDim2 = new UDim2(0.1,0,0.1,0);
-const DEFAULT_SHADE_COLOR: Color3 = Color3.fromRGB(215,0,0);
+const DEFAULT_BTN_COLOR: Color3 = Color3.fromRGB(215,0,0);
+const TWEEN_INFO = new TweenInfo(0.2,Enum.EasingStyle.Quad,Enum.EasingDirection.Out);
 
 interface CloseBtnProps {
 	btnContent?: string;
 	size?: UDim2;
-	shadeColor?: Color3;
+	btnColor?: Color3;
+	bgColor?: Color3;
 	onClick: () => void;
 }
 
-export function CloseBtn(btnProps: CloseBtnProps) {
+type CloseBtnInst = Frame & {
+	Shader: Frame;
+	CloseBtn: ImageButton;
+}
+
+const CloseBtn = forwardRef((btnProps: CloseBtnProps, ref: Ref<CloseBtnInst>) => {
 	const btnContent = btnProps.btnContent ?? DEFAULT_CLOSE;
 	const btnSize = btnProps.size ?? DEFAULT_SIZE;
-	const shadeColor = btnProps.shadeColor ?? DEFAULT_SHADE_COLOR;
+	const btnColor = btnProps.btnColor ?? DEFAULT_BTN_COLOR;
+	const bgColor = btnProps.bgColor ?? new Color3(1,1,1);
 
+	const shaderRef = useRef<Frame>();
 	const btnRef = useRef<ImageButton>();
+
+	// Calculate contrast
+	const shaderColor = useMemo(() => {
+		const lum = bgColor.R * 0.299 + bgColor.G * 0.587 + bgColor.B * 0.114;
+		return lum > 0.5 ? new Color3(0,0,0) : new Color3(1,1,1);
+	},[bgColor]);
+
+	const setShaderOpacity = (transparency: number) => {
+		if (shaderRef.current) TweenService.Create(shaderRef.current,TWEEN_INFO,{ BackgroundTransparency: transparency }).Play();
+	};
+
 	return (
-		<imagebutton
-			key={"CloseBtn"}
-			ref={btnRef}
-			Image={btnContent}
-			ImageColor3={shadeColor}
-			ScaleType={Enum.ScaleType.Fit}
+		// Container Frame
+		<frame
+			key={"CloseBtnContainer"}
 			Size={btnSize}
-			AnchorPoint={new Vector2(1,0)}
-			Position={new UDim2(1,0,0,0)}
 			BackgroundTransparency={1}
-			AutoButtonColor={false}
-			Event={{
-				MouseButton1Click: () => {
-					if (btnRef.current) {
-						const parentInst = btnRef.current.Parent;
-
-						if (parentInst && parentInst.IsA("GuiObject")) {
-							parentInst.Visible = false;
-						}
-
-						// Close the parent Instance of the close button
-						btnProps.onClick();
-					}
-				},
-				MouseEnter: () => {
-					if (!btnRef.current) return;
-
-					
-				},
-				MouseLeave: () => {
-
-				}
-			}}
+			ClipsDescendants={true}
+			BorderSizePixel={0}
 		>
 			<uiaspectratioconstraint
+				key={"UIAspectRatioConstraint"}
+				AspectRatio={1}
 				AspectType={Enum.AspectType.FitWithinMaxSize}
 				DominantAxis={Enum.DominantAxis.Height}
-				AspectRatio={1}
 			/>
-		</imagebutton>
+
+			<frame
+				key={"Shader"}
+				ref={shaderRef}
+				ZIndex={2}
+				Size={UDim2.fromScale(1,1)}
+				BackgroundColor3={shaderColor}
+				BackgroundTransparency={1}
+				BorderSizePixel={0}
+				Active={false}
+			/>
+
+			<imagebutton
+				key={"CloseBtn"}
+				ref={btnRef}
+				ZIndex={1}
+				Size={UDim2.fromScale(1,1)}
+				Image={btnContent}
+				ImageColor3={btnColor}
+				ScaleType={Enum.ScaleType.Fit}
+				AutoButtonColor={false}
+				Event={{
+					MouseButton1Click: () => btnProps.onClick(),
+					MouseEnter: () => setShaderOpacity(0.85),
+					MouseLeave: () => setShaderOpacity(1),
+					MouseButton1Down: () => setShaderOpacity(0.7),
+					MouseButton1Up: () => setShaderOpacity(0.85)
+				}}
+			>
+			</imagebutton>
+		</frame>
 	);
-}
+});
+
+export { CloseBtn, CloseBtnInst };
