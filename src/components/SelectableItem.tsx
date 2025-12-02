a6c0366acfb15fc8fb24766024d97dc11e47abe5
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-02 01:48:19 -0500

    init

---
+++ src/components/SelectableItem.tsx
@@ -0,0 +1,19 @@
+import React from '@rbxts/react';
+import { PropsWithChildren } from '@rbxts/react';
+
+interface SelectableItemProps extends PropsWithChildren {
+	id: string;
+}
+
+export function SelectableItem(props: SelectableItemProps) {
+	return (
+		<textbutton
+			LayoutOrder={1}
+			BackgroundColor3={Color3.fromRGB(60,60,60)}
+			TextXAlignment={Enum.TextXAlignment.Center}
+			Event={{
+				MouseButton1Click: () => selectItem()
+			}}
+		/>
+	);
+}
