7e2f305c23aedff19885f9f44d0c42c2fc4fbfaf
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-01 21:54:10 -0500

    Add useSelectableGroup logic

--- tests/components/SelectableGroup.tsx
+++ tests/components/SelectableGroup.tsx
@@ -1,17 +1,42 @@
 import React from '@rbxts/react';
+import { useSelectableGroup } from '../../src/hooks/useSelectableGroup';
 
 interface SelectableGroupProps {
 
 }
 
 export function SelectableGroup(props: SelectableGroupProps) {
+
+	const {
+		currentSelection, selectItem,
+		isSelected, SelectionChanged
+	} = useSelectableGroup({ isSingleOnly: true, requireSelection: false });
+
 	return (
 		<scrollingframe
 		BackgroundColor3={Color3.fromRGB(105,105,105)}
 		>
 			<uilistlayout/>
 			<textbutton
+			LayoutOrder={1}
+			BackgroundColor3={Color3.fromRGB(60,60,60)}
+			TextXAlignment={Enum.TextXAlignment.Center}
 			Text={"A button."}
+			Event={{
+				MouseButton1Click: () => selectItem()
+			}}
+			/>
+			<textbutton
+			LayoutOrder={2}
+			BackgroundColor3={Color3.fromRGB(60,60,60)}
+			TextXAlignment={Enum.TextXAlignment.Center}
+			Text={"B button."}
+			/>
+			<textbutton
+			LayoutOrder={3}
+			BackgroundColor3={Color3.fromRGB(60,60,60)}
+			TextXAlignment={Enum.TextXAlignment.Center}
+			Text={"C button."}
 			/>
 		</scrollingframe>
 	);
