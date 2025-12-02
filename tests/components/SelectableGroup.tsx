e7fc3874031f0023d6231306d25e7540fe5a8565
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-02 01:48:44 -0500

    Refactor to use SelectableItem directly

--- tests/components/SelectableGroup.tsx
+++ tests/components/SelectableGroup.tsx
@@ -1,5 +1,6 @@
 import React from '@rbxts/react';
 import { useSelectableGroup } from '../../src/hooks/useSelectableGroup';
+import { SelectableItem } from './SelectableItem';
 
 interface SelectableGroupProps {
 
@@ -17,27 +18,7 @@ export function SelectableGroup(props: SelectableGroupProps) {
 		BackgroundColor3={Color3.fromRGB(105,105,105)}
 		>
 			<uilistlayout/>
-			<textbutton
-			LayoutOrder={1}
-			BackgroundColor3={Color3.fromRGB(60,60,60)}
-			TextXAlignment={Enum.TextXAlignment.Center}
-			Text={"A button."}
-			Event={{
-				MouseButton1Click: () => selectItem()
-			}}
-			/>
-			<textbutton
-			LayoutOrder={2}
-			BackgroundColor3={Color3.fromRGB(60,60,60)}
-			TextXAlignment={Enum.TextXAlignment.Center}
-			Text={"B button."}
-			/>
-			<textbutton
-			LayoutOrder={3}
-			BackgroundColor3={Color3.fromRGB(60,60,60)}
-			TextXAlignment={Enum.TextXAlignment.Center}
-			Text={"C button."}
-			/>
+			<SelectableItem/>
 		</scrollingframe>
 	);
 }
