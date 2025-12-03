64725b9b1647b8857a67da27249dec8f277fb8d9
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-03 02:30:43 -0500

    Add Selectable group & items

--- tests/App.tsx
+++ tests/App.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from '@rbxts/react';
 import { CloseBtn } from './CloseBtn';
+import { SelectableGroup } from './SelectableGroup';
+import { SelectableItem } from './components/SelectableItem';
 
 export const App = () => {
 	
@@ -11,11 +13,27 @@ export const App = () => {
 				CanvasSize={new UDim2(0,0,1.5,0)}
 				Position={new UDim2(0.5,0,0.5,0)}
 			>
-				<uigridlayout CellSize={new UDim2(0,200,0,200)}/>
+				<uigridlayout
+				CellSize={new UDim2(0,200,0,200)}
+				/>
 				
 				<CloseBtn
+					size={UDim2.fromOffset(200,200)}
 					onClose={() => {}}
 				/>
+				<SelectableGroup
+					config={{requireSelection: true,isSingleOnly: true}}
+				>
+				<SelectableItem
+					id="button_A"
+				/>
+				<SelectableItem
+					id="button_B"
+				/>
+				<SelectableItem
+					id="button_C"
+				/>
+				</SelectableGroup>
 
 			</scrollingframe>
 		</screengui>
