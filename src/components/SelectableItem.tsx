390291621754f0c1b5ec63a987f9cc35de0e1942
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-03 01:08:33 -0500

    Conditionally apply the right border size & color based on if it's a selected item

--- src/components/SelectableItem.tsx
+++ src/components/SelectableItem.tsx
@@ -6,16 +6,34 @@ interface SelectableItemProps extends PropsWithChildren {
 	id: string;
 }
 
+const DEFAULT_STYLE = {
+	BorderSizePixel: 1,
+	BorderColor3: new Color3(0,0,0)
+};
+const SELECTED_STYLE = {
+	BorderSizePixel: 2,
+	BorderColor3: new Color3(1,1,1)
+};
+
 export function SelectableItem(props: SelectableItemProps) {
 	const { selectItem, isSelected } = useContext(SelectableGroupContext);
+
+	const isCurrentlySelected: boolean = isSelected(props.id);
+	
+	const handleClick = React.useCallback(() => {
+		selectItem(props.id);
+	},[props.id,selectItem]);
 
 	return (
 		<textbutton
 			LayoutOrder={1}
 			BackgroundColor3={Color3.fromRGB(60,60,60)}
 			TextXAlignment={Enum.TextXAlignment.Center}
+
+			{...(isCurrentlySelected ? SELECTED_STYLE : DEFAULT_STYLE)}
+
 			Event={{
-				MouseButton1Click: () => selectItem(props.id)
+				MouseButton1Click: () => handleClick
 			}}
 		/>
 	);
