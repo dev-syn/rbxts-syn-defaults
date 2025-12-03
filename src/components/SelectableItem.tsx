ded958cc769f5eec07cd069f7471442819c86397
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-03 04:06:11 -0500

    Add support for ImageButtons

--- src/components/SelectableItem.tsx
+++ src/components/SelectableItem.tsx
@@ -2,9 +2,20 @@ import React, { useContext } from '@rbxts/react';
 import { PropsWithChildren } from '@rbxts/react';
 import { SelectableGroupContext } from '../context/SelectableGroupContext';
 
-interface SelectableItemProps extends PropsWithChildren {
+interface BaseSelectableItemProps {
 	id: string;
+	btnType?: "TextButton" | "ImageButton"
 }
+
+interface TextModeProps extends BaseSelectableItemProps, React.InstanceProps<TextButton> {
+	btnType: "TextButton"
+}
+
+interface ImageModeProps extends BaseSelectableItemProps, React.InstanceProps<ImageButton> {
+	btnType: "ImageButton"
+}
+
+export type SelectableItemProps = TextModeProps | ImageModeProps;
 
 const DEFAULT_STYLE = {
 	BorderSizePixel: 1,
@@ -15,26 +26,52 @@ const SELECTED_STYLE = {
 	BorderColor3: new Color3(1,1,1)
 };
 
+const DEFAULT_BG: Color3 = Color3.fromRGB(60,60,60);
+
 export function SelectableItem(props: SelectableItemProps) {
 	const { selectItem, isSelected } = useContext(SelectableGroupContext);
 
+	const type = props.btnType ?? "TextButton";
 	const isCurrentlySelected: boolean = isSelected(props.id);
-	
+
 	const handleClick = React.useCallback(() => {
 		selectItem(props.id);
 	},[props.id,selectItem]);
 
-	return (
-		<textbutton
-			LayoutOrder={1}
-			BackgroundColor3={Color3.fromRGB(60,60,60)}
-			TextXAlignment={Enum.TextXAlignment.Center}
+	const eventProps = {
+		MouseButton1Click: handleClick
+	};
 
-			{...(isCurrentlySelected ? SELECTED_STYLE : DEFAULT_STYLE)}
+	if (type === "ImageButton") {
+		const { id,btnType,...nativeProps } = props as ImageModeProps;
 
-			Event={{
-				MouseButton1Click: () => handleClick
-			}}
-		/>
-	);
+		return (
+			<imagebutton
+				{...nativeProps}
+
+				LayoutOrder={1}
+				BackgroundColor3={DEFAULT_BG}
+
+				{...(isCurrentlySelected ? SELECTED_STYLE : DEFAULT_STYLE)}
+
+				Event={eventProps}
+			/>
+		);
+	} else {
+		const { id,btnType,...nativeProps } = props as TextModeProps;
+
+		return (
+			<textbutton
+				{...nativeProps}
+
+				LayoutOrder={1}
+				BackgroundColor3={DEFAULT_BG}
+				TextXAlignment={Enum.TextXAlignment.Center}
+
+				{...(isCurrentlySelected ? SELECTED_STYLE : DEFAULT_STYLE)}
+
+				Event={eventProps}
+			/>
+		);
+	}
 }
