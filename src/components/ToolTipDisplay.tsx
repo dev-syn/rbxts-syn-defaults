a2e255a0afbacea1ce57698e7fc0a6d176f5f15a
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-04 04:14:33 -0500

    Rename ToolTipDisplay

---
+++ src/components/ToolTipDisplay.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from '@rbxts/react';
+import { ToolTipState } from '../hooks/useToolTip';
+
+interface ToolTipProps {
+	data: ToolTipState;
+	/** Whether this ToolTip contains a background image or is text based. */
+	imageContent?: string;
+}
+
+const DEFAULT_TEXT_SIZE = UDim2.fromOffset(120,30);
+const DEFAULT_IMAGE_SIZE = UDim2.fromOffset(150,150);
+
+export function ToolTipDisplay({ data,imageContent }: ToolTipProps) {
+
+	const hasImageBackground: boolean = imageContent !== undefined && imageContent.size() > 0;
+	const contentSize = hasImageBackground ? DEFAULT_IMAGE_SIZE : DEFAULT_TEXT_SIZE;
+
+	const posOffset = UDim2.fromOffset(data.pos.X,data.pos.Y);
+
+	let textStr = tostring(data.content);
+
+	const children: React.ReactElement[] = [];
+
+	if (hasImageBackground && imageContent) {
+		children.push(
+			<imagelabel
+				key="ImageBackground"
+				Image={imageContent}
+				Size={UDim2.fromScale(1,1)}
+				BackgroundTransparency={1}
+				ZIndex={1}
+				ScaleType={Enum.ScaleType.Slice}
+				SliceCenter={new Rect(10,10,10,10)}
+			/>
+		);
+	}
+
+	children.push(
+		<textlabel
+			key={"TextDisplay"}
+			Text={textStr}
+			Size={UDim2.fromScale(1, 1)}
+			BackgroundTransparency={1}
+			Font={Enum.Font.SourceSans}
+			TextColor3={new Color3(1,1,1)}
+			TextSize={14}
+			TextWrapped={true}
+			TextXAlignment={Enum.TextXAlignment.Center}
+			TextYAlignment={Enum.TextYAlignment.Center}
+			ZIndex={2}
+		/>
+	);
+	
+	return (
+		<frame
+			key={"ToolTipFrame"}
+			AnchorPoint={new Vector2(0.5,1)}
+			Position={posOffset}
+			Size={contentSize}
+			BackgroundColor3={Color3.fromRGB(75,75,75)}
+			BackgroundTransparency={0}
+			BorderSizePixel={0}
+			ZIndex={1000}
+		>
+			{children}
+		</frame>
+	);
+}
