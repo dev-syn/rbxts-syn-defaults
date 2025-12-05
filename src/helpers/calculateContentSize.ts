070e623c689b167a2cc3dfe15f0edd71bc757934
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-05 16:44:17 -0500

    Support both Enum.Font and the Font object

--- src/helpers/calculateContentSize.ts
+++ src/helpers/calculateContentSize.ts
@@ -1,18 +1,47 @@
 import { TextService } from '@rbxts/services';
 
-interface TextProps {
+export interface TextProps {
 	text: string;
-	fontSize: number;
+	textSize: number;
 	font: Font | Enum.Font;
-	lineHeight?: number;
+	richText?: boolean;
 }
 
-export function calculateContentSize(props: TextProps,bounds: Vector2) {
+function isFontObject(val: unknown): val is Font {
+	return val instanceof Font;
+}
+
+/** Calculates how much a font with a size would take up.
+ * You can provide a Enum.Font or a Font object itself.
+ * NOTE: When providing a Font object this method can yield especially when the Font is not already loaded.
+ */
+export function calculateContentSize(props: TextProps,bounds: Vector2): Vector2 {
 	if (!props.font) return new Vector2(0,0);
-	return TextService.GetTextSize(
-		props.text,
-		props.fontSize,
-		props.font,
-		bounds
-	);
+
+	if (isFontObject(props.font)) {
+		// We know it's a Font object
+		const textBoundParams: GetTextBoundsParams = new Instance("GetTextBoundsParams");
+		textBoundParams.Font = props.font;
+		textBoundParams.RichText = !!props.richText;
+		textBoundParams.Size = props.textSize;
+		textBoundParams.Text = props.text;
+		textBoundParams.Width = bounds.X;
+
+		const [success,sizeOrErr] = pcall(() => {
+			TextService.GetTextBoundsAsync(textBoundParams);
+		});
+		if (success) return sizeOrErr as unknown as Vector2;
+		else {
+			warn(`calculateContentSize failed for Font object: ${sizeOrErr}`);
+			return new Vector2(0,0);
+		}
+	} else {
+		// Let's assume a Enum.Font
+		return TextService.GetTextSize(
+			props.text,
+			props.textSize,
+			props.font,
+			bounds
+		);
+	}
 }
