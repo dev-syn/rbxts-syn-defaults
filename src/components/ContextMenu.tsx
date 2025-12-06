dbaf9257b2cbaaf7cfc827ac2f278fbc0f043c89
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-06 10:00:33 -0500

    Add id register and unregister

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,13 +1,36 @@
-import React from '@rbxts/react';
+import React, { PropsWithChildren, useMemo, useState } from '@rbxts/react';
+import { ContextMenuContext, ContextRegistration } from '../context/ContextMenuContext';
 
-export function ContextMenu() {
+export interface ContextMenuProps extends PropsWithChildren {
+	menuId: string;
+}
+
+export function ContextMenu({ menuId,children }: ContextMenuProps) {
+	const [itemIds,setItemIds] = useState<Array<string | number>>([]);
+
+	const contextVal: ContextRegistration = useMemo(() => {
+		const registerItemId = (id: string | number) => {
+			setItemIds(prevIds => {
+				if (!prevIds.includes(id)) return [...prevIds,id];
+				return prevIds;
+			})
+		}
+
+		const unregisterItemId = (id: string | number) =>
+			setItemIds(prevIds => prevIds.filter(itemId => itemId !== id));
+
+		return { registerItemId,unregisterItemId };
+	},[]);
 	
 	return (
-		<scrollingframe
-			CanvasSize={new UDim2(0,0,0,0)}
-		>
-
+		<ContextMenuContext.Provider value={contextVal}>
+			<scrollingframe
+				Size={UDim2.fromOffset(400,400)}
+				CanvasSize={new UDim2(0,0,0,0)}
+			>
+			{children}
 			<uilistlayout/>
-		</scrollingframe>
+			</scrollingframe>
+		</ContextMenuContext.Provider>
 	);
 }
