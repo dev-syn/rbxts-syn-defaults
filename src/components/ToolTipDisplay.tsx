76c9389028ff1e4e2d5848688bdeb31d844bfd2a
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-06 04:39:03 -0500

    Add Font object and binding for the Position

--- src/components/ToolTipDisplay.tsx
+++ src/components/ToolTipDisplay.tsx
@@ -1,21 +1,22 @@
 import React, { useState } from '@rbxts/react';
 import { ToolTipState } from '../hooks/useToolTip';
+import { CalculateContentProps, isFontObject } from '../helpers/calculateContentSize';
 
 interface ToolTipProps {
 	data: ToolTipState;
 	/** Whether this ToolTip contains a background image or is text based. */
 	imageContent?: string;
+	calculateContentProps: Omit<CalculateContentProps,'text'>;
 }
 
 const DEFAULT_TEXT_SIZE = UDim2.fromOffset(120,30);
 const DEFAULT_IMAGE_SIZE = UDim2.fromOffset(150,150);
 
-export function ToolTipDisplay({ data,imageContent }: ToolTipProps) {
+export function ToolTipDisplay({ data,imageContent,calculateContentProps }: ToolTipProps) {
 
 	const hasImageBackground: boolean = imageContent !== undefined && imageContent.size() > 0;
 	const contentSize = hasImageBackground ? DEFAULT_IMAGE_SIZE : DEFAULT_TEXT_SIZE;
 
-	const posOffset = UDim2.fromOffset(data.pos.X,data.pos.Y);
 	let textStr = tostring(data.content);
 
 	const children: React.ReactElement[] = [];
@@ -34,31 +35,37 @@ export function ToolTipDisplay({ data,imageContent }: ToolTipProps) {
 		);
 	}
 
+
+	const fontProps: { Font?: Enum.Font,FontFace?: Font } = {};
+	if (isFontObject(calculateContentProps.font)) fontProps.FontFace = calculateContentProps.font;
+	else fontProps.Font =  calculateContentProps.font;
+	
 	children.push(
 		<textlabel
 			key={"TextDisplay"}
 			Text={textStr}
 			Size={UDim2.fromScale(1,1)}
 			BackgroundTransparency={1}
-			Font={Enum.Font.SourceSans}
 			TextColor3={new Color3(1,1,1)}
 			TextSize={16}
 			TextWrapped={true}
 			TextXAlignment={Enum.TextXAlignment.Center}
 			TextYAlignment={Enum.TextYAlignment.Center}
 			ZIndex={2}
+
+			{...fontProps}
 		/>
 	);
-	
+
 	return (
 		<frame
 			key={"ToolTipFrame"}
-			Position={posOffset}
-			Size={contentSize}
+			Size={UDim2.fromOffset(data.calculatedSize.X,data.calculatedSize.Y)}
 			BackgroundColor3={Color3.fromRGB(75,75,75)}
 			BackgroundTransparency={0}
 			BorderSizePixel={0}
 			ZIndex={0}
+			Position={data.bind_pos}
 		>
 			{children}
 		</frame>
