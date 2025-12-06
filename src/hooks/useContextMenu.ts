9473f2e44b2a3049af776a93ef1f3325f31928c3
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-06 07:44:39 -0500

    Fix typo in file name

---
+++ src/hooks/useContextMenu.ts
@@ -0,0 +1,9 @@
+type ContextItemData = string;
+
+export interface ContextMenuConfig {
+	/** Whether this ContextMenu can be right-clicked and activated
+	 * anywhere even when no trigger element is present.
+	 */
+	activateAnywhere: boolean;
+}
+
