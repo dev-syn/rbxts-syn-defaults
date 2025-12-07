f7370632b64d664d610eb58cc09b6658c06c1ba0
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-06 21:29:33 -0500

    Add properties into ui elements

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,35 +1,26 @@
 import React, { PropsWithChildren, useMemo, useState } from '@rbxts/react';
 import { ContextMenuContext, ContextRegistration } from '../context/ContextMenuContext';
+import { useContextMenu } from '../hooks/useContextMenu';
 
 export interface ContextMenuProps extends PropsWithChildren {
 	menuId: string;
 }
 
 export function ContextMenu({ menuId,children }: ContextMenuProps) {
-	const [itemIds,setItemIds] = useState<Array<string | number>>([]);
-
-	const contextVal: ContextRegistration = useMemo(() => {
-		const registerItemId = (id: string | number) => {
-			setItemIds(prevIds => {
-				if (!prevIds.includes(id)) return [...prevIds,id];
-				return prevIds;
-			})
-		}
-
-		const unregisterItemId = (id: string | number) =>
-			setItemIds(prevIds => prevIds.filter(itemId => itemId !== id));
-
-		return { registerItemId,unregisterItemId };
-	},[]);
+	const { contextVal, itemIds } = useContextMenu({ activateAnywhere: false });
 	
 	return (
 		<ContextMenuContext.Provider value={contextVal}>
 			<scrollingframe
 				Size={UDim2.fromOffset(400,400)}
-				CanvasSize={new UDim2(0,0,0,0)}
+				AutomaticCanvasSize={Enum.AutomaticSize.Y}
+				ScrollingDirection={Enum.ScrollingDirection.Y}
 			>
 			{children}
-			<uilistlayout/>
+			<uilistlayout
+				FillDirection={Enum.FillDirection.Vertical}
+				SortOrder={Enum.SortOrder.LayoutOrder}
+			/>
 			</scrollingframe>
 		</ContextMenuContext.Provider>
 	);
