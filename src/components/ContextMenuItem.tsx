04645eca09d0042a2c74ec73657d0ae4073be45c
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-06 21:29:09 -0500

    Add getLayoutOrder usage

--- src/components/ContextMenuItem.tsx
+++ src/components/ContextMenuItem.tsx
@@ -1,17 +1,36 @@
-import React, { useContext } from '@rbxts/react';
+import React, { useContext, useEffect } from '@rbxts/react';
 import { ContextMenuContext } from '../context/ContextMenuContext';
 
 export type ContextItemData = string;
 
-interface ContextItemProps {
+interface ContextItemProps extends Partial<React.ComponentPropsWithoutRef<'textlabel'>>{
 	id: string | number;
+	text: string;
 }
 
-export function ContextMenuItem({ id }: ContextItemProps) {
-	const { registerItemId,unregisterItemId } = useContext(ContextMenuContext);
+export function ContextMenuItem(props: ContextItemProps) {
+	const { registerItemId,unregisterItemId,getLayoutOrder } = useContext(ContextMenuContext);
+
+	const id = props.id;
+	const text = props.text;
+
+	useEffect(() => {
+		registerItemId(id);
+		return () => unregisterItemId(id);
+	},[id,registerItemId,unregisterItemId]);
+
+	const nativeProps = { ...props } as Record<string,unknown>;
+	delete nativeProps.id;
+	delete nativeProps.text;
+
+	const layoutOrder = getLayoutOrder ? getLayoutOrder(id) : 0;
+
 	return (
 		<textlabel
-
+			{...nativeProps}
+			LayoutOrder={layoutOrder}
+			Size={new UDim2(1,0,0,30)}
+			Text={`{Item: ${id}} - ${text}`}
 		/>
 	);
 }
