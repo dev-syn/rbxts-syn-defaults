83cefaead92322b378d39801d8f524ce876e72f6
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-10 06:31:48 -0500

    Plug in the sample logic for the SampleContextMenu

--- tests/SampleContextMenu.tsx
+++ tests/SampleContextMenu.tsx
@@ -1,36 +1,41 @@
 import React from '@rbxts/react';
 import { ContextMenu } from './components/ContextMenu';
 import { ContextMenuItem } from './components/ContextMenuItem';
+import { ContextMenuTriggerBtn } from './components/ContextMenuTriggerBtn';
 
+const CONTEXT_MENU_ID = "SAMPLE_CONTEXT_MENU";
 export function SampleContextMenu() {
 
 	return (
-		<ContextMenu
-			menuId='sample_context_menu'
-		>
-			<ContextMenuItem
-				id='item_1'
-				text="Sample 1"
-				onActivate={() => {
-					print(`Inside item_1`);
-				}}
-			/>
+		<>
+			<ContextMenuTriggerBtn menuId={CONTEXT_MENU_ID} Text="Right-click me to open the context menu."/>
+			<ContextMenu
+				menuId={CONTEXT_MENU_ID}
+			>
+				<ContextMenuItem
+					id='item_1'
+					text="Sample 1"
+					onActivate={() => {
+						print(`Inside item_1`);
+					}}
+				/>
 			
-			<ContextMenuItem
-				id='item_2'
-				text="Sample 2"
-				onActivate={() => {
-					print(`Inside item_2`);
-				}}
-			/>
+				<ContextMenuItem
+					id='item_2'
+					text="Sample 2"
+					onActivate={() => {
+						print(`Inside item_2`);
+					}}
+				/>
 
-			<ContextMenuItem
-				id='item_3'
-				text="Sample 3"
-				onActivate={() => {
-					print(`Inside item_3`);
-				}}
-			/>
-		</ContextMenu>
+				<ContextMenuItem
+					id='item_3'
+					text="Sample 3"
+					onActivate={() => {
+						print(`Inside item_3`);
+					}}
+				/>
+			</ContextMenu>
+		</>
 	);
 }
