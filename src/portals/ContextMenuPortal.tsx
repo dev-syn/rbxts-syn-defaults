72c83187c1c35e216c78f6f22d0220ace9790f93
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-11 13:48:16 -0500

    Add positioning and size calculations

--- src/portals/ContextMenuPortal.tsx
+++ src/portals/ContextMenuPortal.tsx
@@ -1,17 +1,25 @@
-import { PropsWithChildren, PropsWithRef, Ref, useEffect, useRef, useState } from '@rbxts/react';
+import { ComponentPropsWithRef, PropsWithChildren, PropsWithRef, Ref, useEffect, useLayoutEffect, useRef, useState } from '@rbxts/react';
 import { createPortal } from '@rbxts/react-roblox';
 import { ContextMenuRoot } from '../misc/ContextMenuRoot';
 import React from '@rbxts/react';
 
-interface ContextMenuProps extends PropsWithChildren {
+interface ContextMenuProps extends PropsWithChildren,
+Partial<ComponentPropsWithRef<'scrollingframe'>> {
 	triggerNode?: GuiObject;
 }
 
-export function ContextMenuPortal({ triggerNode, children }: ContextMenuProps) {
+export function ContextMenuPortal(props: ContextMenuProps) {
+	const triggerNode = props.triggerNode;
+	const children = props.children;
+
 	const ref = useRef<ScrollingFrame>(undefined);
 	const [position,setPosition] = useState<UDim2>(new UDim2(0,0,0,0));
+	const [size,setSize] = useState<UDim2>(new UDim2(0,250,0,300));
+
+	const listLayoutRef = useRef<UIListLayout>();
 
 	useEffect(() => {
+		print(`Ref current: ${ref.current}, triggerNode: ${triggerNode}`);
 		if (!ref.current || !triggerNode) return;
 
 		const menuSize = ref.current.AbsoluteSize;
@@ -31,20 +39,35 @@ export function ContextMenuPortal({ triggerNode, children }: ContextMenuProps) {
 		const yOffset = isSizeOverHalfY ? topAbsPosY - menuSize.Y : topAbsPosY;
 
 		setPosition(UDim2.fromOffset(xOffset,yOffset));
-	},[triggerNode]);
+	},[listLayoutRef,listLayoutRef.current?.AbsoluteContentSize,triggerNode,size]);
+
+	useEffect(() => {
+		if (!listLayoutRef.current) return;
+
+		const absSize = listLayoutRef.current.AbsoluteContentSize;
+
+		setSize(UDim2.fromOffset(absSize.X,absSize.Y));
+
+	},[listLayoutRef,listLayoutRef.current?.AbsoluteContentSize,triggerNode,position]);
+
+	const nativeProps = {...props};
+	delete nativeProps.triggerNode;
 
 	return createPortal(
 		<scrollingframe
 			ref={ref}
-			Position={UDim2.fromOffset(position.X.Offset,position.Y.Offset)}
-			Size={UDim2.fromScale(1,1)}
 
+			{...nativeProps}
+
+			Position={UDim2.fromOffset(position.X.Offset,position.Y.Offset)}
+			Size={UDim2.fromOffset(size.X.Offset,size.Y.Offset)}
 			CanvasSize={new UDim2(0,0,0,0)}
 			AutomaticCanvasSize={Enum.AutomaticSize.Y}
 			ScrollingDirection={Enum.ScrollingDirection.Y}
 		>
 			{children}
 			<uilistlayout
+				ref={listLayoutRef}
 				FillDirection={Enum.FillDirection.Vertical}
 				SortOrder={Enum.SortOrder.LayoutOrder}
 			/>
