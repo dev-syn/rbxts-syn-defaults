3ca2e9cf5fbe380a19bba1e14bb9a8c6fbccf0d9
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-11 03:33:10 -0500

    Add handleMenuOpen logic (not fixed)

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,29 +1,85 @@
-import React, { PropsWithChildren, useMemo, useState } from '@rbxts/react';
-import { ContextMenuContext, ContextRegistration } from '../context/ContextMenuContext';
+import React, { PropsWithChildren, useCallback, useEffect, useRef, useState } from '@rbxts/react';
+import { ContextMenuContext, ContextMenuID } from '../context/ContextMenuContext';
 import { useContextMenu } from '../hooks/useContextMenu';
+import { onOpenContextMenu } from '../registries/menuRegistry';
+import { ContextMenuPortal } from '../portals/ContextMenuPortal';
 
 export interface ContextMenuProps extends PropsWithChildren {
 	menuId: string;
 }
 
 export function ContextMenu({ menuId,children }: ContextMenuProps) {
-	const { contextVal, itemIds } = useContextMenu({ menuId: menuId,config: { activateAnywhere: false } });
+	const [open, setOpen] = useState(false);
+	const [pos, setPos] = useState<UDim2>(new UDim2(0,0,0,0));
+	const [activeTrigger, setActiveTrigger] = useState<ContextMenuID | undefined>(undefined);
+
+	const { contextVal,itemIds } = useContextMenu({
+		menuId: menuId,
+		config: { activateAnywhere: false }
+	});
+
+	const menuRef = useRef<ScrollingFrame>();
+
+	const handleMenuOpen = useCallback((menuId: ContextMenuID,node?: GuiObject) => {
+		if (!menuRef.current) {
+			print("No ref");
+			return;
+		}
+		const contextMenu = menuRef.current;
+
+		if (open) {
+			print(`Closing context menu: ${menuId}`);
+			setOpen(false);
+			return;
+		}
+
+		if (node) {
+			const viewportSize: Vector2 = game.Workspace.CurrentCamera!.ViewportSize;
+
+			const absSizeY: number = node.AbsoluteSize.Y;
+			const absPosY: number = node.AbsolutePosition.Y;
+
+			const yAnchor: number = node.AnchorPoint.Y;
+
+			const topAbsPosY: number = math.ceil(absPosY - absSizeY * yAnchor);
+			const leftAbsPosX = node.AbsolutePosition.X - math.ceil(node.AbsoluteSize.X * node.AnchorPoint.X);
+			const rightAbsPosX = leftAbsPosX + contextMenu.AbsoluteSize.X;
+
+			// Position the menu before open
+			const isSizeOverHalfX: boolean = leftAbsPosX > viewportSize.X / 2;
+			const isSizeOverHalfY: boolean = absPosY > viewportSize.Y / 2;
+
+			const xOffset = isSizeOverHalfX ?
+			leftAbsPosX - contextMenu.AbsoluteSize.X
+			: rightAbsPosX + (node.AbsoluteSize.X - contextMenu.AbsoluteSize.X);
+
+			const yOffset = isSizeOverHalfY ?
+			(topAbsPosY + -(contextMenu.AbsoluteSize.Y - math.ceil(1 / viewportSize.Y) * contextMenu.AbsoluteSize.Y))
+			: topAbsPosY;
+
+			setPos(UDim2.fromOffset(xOffset,yOffset));
+		}
+
+		print(`Opening context menu: ${menuId}`);
+		setOpen(true);
+	},[open,pos]);
+
+	useEffect(() => {
+		onOpenContextMenu(menuId,handleMenuOpen);
+		return () => onOpenContextMenu(menuId,undefined);
+	},[menuId,handleMenuOpen]);
+
 	
 	return (
 		<ContextMenuContext.Provider value={contextVal}>
-			<scrollingframe
+			{open && 
+			<ContextMenuPortal
+				menuRef={menuRef}
 				key={`ContextMenu-${menuId}`}
-				Size={UDim2.fromOffset(400,400)}
-				CanvasSize={new UDim2(0,0,0,0)}
-				AutomaticCanvasSize={Enum.AutomaticSize.Y}
-				ScrollingDirection={Enum.ScrollingDirection.Y}
+				position={pos}
 			>
-			{children}
-			<uilistlayout
-				FillDirection={Enum.FillDirection.Vertical}
-				SortOrder={Enum.SortOrder.LayoutOrder}
-			/>
-			</scrollingframe>
+				{children}
+			</ContextMenuPortal>}
 		</ContextMenuContext.Provider>
 	);
 }
