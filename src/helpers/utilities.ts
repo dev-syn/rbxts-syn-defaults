f7109d655d0cb1b251cd1c2a9760a483ba4f0ae0
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-12 16:25:30 -0500

    init

---
+++ src/helpers/utilities.ts
@@ -0,0 +1,9 @@
+import { Players } from '@rbxts/services';
+
+const player = Players.LocalPlayer;
+
+function getPlayerGui(): Promise<PlayerGui> {
+	return Promise.try<PlayerGui>(() => player.WaitForChild("PlayerGui") as PlayerGui);
+}
+
+export { getPlayerGui };
