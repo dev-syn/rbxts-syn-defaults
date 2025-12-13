6f564e44f2c969af5aaf855cec95a15f4210e899
Casey <103451081+dev-syn@users.noreply.github.com>
2025-12-13 05:08:55 -0500

    Fix getPlayerGui with Promise.try

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -3,7 +3,9 @@ import { Players } from '@rbxts/services';
 const player = Players.LocalPlayer;
 
 function getPlayerGui(): Promise<PlayerGui> {
-	return Promise.try<PlayerGui>(() => player.WaitForChild("PlayerGui") as PlayerGui);
+	return Promise.try<PlayerGui>(
+		() => player.WaitForChild("PlayerGui") as PlayerGui
+	);
 }
 
 export { getPlayerGui };
